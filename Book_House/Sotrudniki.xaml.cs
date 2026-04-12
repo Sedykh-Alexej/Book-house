a0d45ee5b4bc38b2a2a6b38c5fc48090128e4876
Седых Алексей <sedx.alexej.4948@mail.ru>
2026-04-12 09:59:11 +0300

    final

--- Book_House/Sotrudniki.xaml.cs
+++ Book_House/Sotrudniki.xaml.cs
@@ -60,7 +60,7 @@ namespace Book_House
 
         private void Exit(object sender, RoutedEventArgs e)
         {
-            Manager.Forma.Navigate(new Сashier());
+            Manager.Forma.Navigate(new Chief());
         }
     }
 }
